0a1ecc904bed0008be0ec835170248e3d9fa490b
Vinicius Souza <visouza@microsoft.com>
2025-07-16 15:35:45 -0400

    chore: rename plugin

--- src/extension.ts
+++ src/extension.ts
@@ -7,16 +7,16 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld-sample" is now active!');
-
+	console.log('Congratulations, your extension "camadazero-analyzer" is now active!');
+	
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('camadazero.analyze', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		vscode.window.showInformationMessage('CamadaZero Extension was activated!');
 	});
 
 	context.subscriptions.push(disposable);
